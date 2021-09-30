ddffad89956a62808c3e1ddc6553afd2c925d8d3
OHLSSON12345678910 <isac.ohlsson@elev.ga.lbs.se>
2021-09-30 15:08:48 +0200

    Shooting script forttsättning / ramp

--- Isac O FPS/Assets/Pistol.cs
+++ Isac O FPS/Assets/Pistol.cs
@@ -4,15 +4,21 @@ public class Pistol : MonoBehaviour {
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f; 
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update() {
         
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) 
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
 
@@ -32,6 +38,14 @@ public class Pistol : MonoBehaviour {
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+          GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
